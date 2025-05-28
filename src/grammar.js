410d2cb6111f3e4ee4129c68780fe9cd3997075c
Chris Lloyd <chris@chrislloyd.net>
2025-05-28 12:39:27 -0700

    Use typescript in grammar

--- src/grammar.js
+++ src/grammar.js
@@ -1,44 +1,47 @@
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
 module.exports = grammar({
   name: "hledger",
 
   rules: {
-    source_file: $ => repeat(choice($.transaction, "\n", $.comment)),
+    source_file: ($) => repeat(choice($.transaction, "\n", $.comment)),
 
-    transaction: $ => seq($.entry, repeat1(seq(/\s{1,}/, $.posting, "\n"))),
+    transaction: ($) => seq($.entry, repeat1(seq(/\s{1,}/, $.posting, "\n"))),
 
-    entry: $ =>
+    entry: ($) =>
       seq(
         $.date,
         optional(seq(/\s*/, $.status)),
         optional($.payee),
-        optional(seq(/\s*/, $.comment))
+        optional(seq(/\s*/, $.comment)),
       ),
 
-    date: $ => {
+    date: ($) => {
       function createDate(separator) {
         return seq(optional(seq($.year, separator)), $.month, separator, $.day);
       }
       return choice(createDate("-"), createDate("/"), createDate("."));
     },
-    year: $ => /\d{4}/,
-    month: $ => /\d{2}/,
-    day: $ => /\d{2}/,
+    year: ($) => /\d{4}/,
+    month: ($) => /\d{2}/,
+    day: ($) => /\d{2}/,
 
-    payee: $ => /[^\n;]+/,
+    payee: ($) => /[^\n;]+/,
 
-    posting: $ =>
+    posting: ($) =>
       seq(
         optional(seq($.status, " ")),
         $.account,
-        optional(seq(/\s{2,}/, $.amount))
+        optional(seq(/\s{2,}/, $.amount)),
       ),
 
-    status: $ => choice("*", "!"),
+    status: ($) => choice("*", "!"),
 
-    account: $ => seq($._accountPart, repeat(seq(":", $._accountPart))),
-    _accountPart: $ => /\w+/,
+    account: ($) => seq($._accountPart, repeat(seq(":", $._accountPart))),
+    _accountPart: ($) => /\w+/,
 
-    amount: $ => seq($.commodity, $.quantity),
+    amount: ($) => seq($.commodity, $.quantity),
 
     // choice(
     //   $.quantity,
@@ -46,9 +49,9 @@ module.exports = grammar({
     //   seq($.quantity, $.commodity) // right commodity
     // ),
 
-    commodity: $ => /[\w\$]+/,
-    quantity: $ => /\d+/,
+    commodity: ($) => /[\w\$]+/,
+    quantity: ($) => /\d+/,
 
-    comment: $ => seq(";", /[^\n]*/)
-  }
+    comment: ($) => seq(";", /[^\n]*/),
+  },
 });
